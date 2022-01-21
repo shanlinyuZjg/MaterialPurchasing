f2e4631dd130e62aefb9c46f289b68aeaf4d06e9
linyu shan <shanlinyu@outlook.com>
2022-01-21 10:48:32 +0800

    AssemblyInfo

--- FrmMain/Properties/AssemblyInfo.cs
+++ FrmMain/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("供应无纸化办公")]
-[assembly: AssemblyDescription("2022-01-20 内销包材计划与供应、物管对接完成，需求数量<=0,优化")]
+[assembly: AssemblyDescription("2022-01-21 增加计划历史查找")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("淄博鸿浩软件有限公司")]
 [assembly: AssemblyProduct("供应无纸化办公")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.5.52.0")]
+[assembly: AssemblyFileVersion("1.5.53.0")]
