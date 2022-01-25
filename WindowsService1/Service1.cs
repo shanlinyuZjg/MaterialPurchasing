fa3ed975f17272a1050a2aa855485ff5f27f5078
linyu shan <shanlinyu@outlook.com>
2022-01-25 16:53:44 +0800

    新增WindowsService1

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        string filePath = @"D:\MyServiceLog.txt";
+        //定时器
+        System.Timers.Timer tmBak = new System.Timers.Timer();
+        //服务器启动时写日志、开启定时器
+        protected override void OnStart(string[] args)
+        {
+            using (FileStream stream = new FileStream(filePath, FileMode.Append))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "服务启动！");
+            }
+            //到时间的时候执行事件 
+            tmBak.Interval = 2000;//2S执行一次
+            tmBak.AutoReset = true;//执行一次 false，一直执行true 
+            //是否执行System.Timers.Timer.Elapsed事件 
+            tmBak.Enabled = true;
+            tmBak.Start();
+            tmBak.Elapsed += new System.Timers.ElapsedEventHandler(WriteLog);
+
+        }
+
+        protected void WriteLog(object source, System.Timers.ElapsedEventArgs e)
+        {
+            using (FileStream stream = new FileStream(filePath, FileMode.Append))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "执行定时任务写操作！");
+            }
+        }
+
+        //服务停止时写日志
+        protected override void OnStop()
+        {
+            using (FileStream stream = new FileStream(filePath, FileMode.Append))
+            using (StreamWriter writer = new StreamWriter(stream))
+            {
+                writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "服务停止！");
+            }
+        }
+    }
+}
