bd688a0aae405363b629d531a274e22c36102a4b
linyu shan <shanlinyu@outlook.com>
2022-05-18 16:29:08 +0800

    11

--- FrmMain/Properties/AssemblyInfo.cs
+++ FrmMain/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("供应无纸化办公")]
-[assembly: AssemblyDescription("2022-05-11 订单下达比价范围修改,函数")]
+[assembly: AssemblyDescription("2022-05-18记录样式")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("淄博鸿浩软件有限公司")]
 [assembly: AssemblyProduct("供应无纸化办公")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.5.68.0")]
+[assembly: AssemblyFileVersion("1.5.69.0")]
