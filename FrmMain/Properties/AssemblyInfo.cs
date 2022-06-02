3b0905b67dcd2aad2ffd927a723e58c2852cb495
linyu shan <shanlinyu@outlook.com>
2022-06-02 16:42:39 +0800

    2022-06-02 批量打印供应商批号生产商批号空格

--- FrmMain/Properties/AssemblyInfo.cs
+++ FrmMain/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("供应无纸化办公")]
-[assembly: AssemblyDescription("2022-06-02 已确认到货增加库管员")]
+[assembly: AssemblyDescription("2022-06-02 批量打印供应商批号生产商批号空格")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("淄博鸿浩软件有限公司")]
 [assembly: AssemblyProduct("供应无纸化办公")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.5.72.0")]
+[assembly: AssemblyFileVersion("1.5.73.0")]
